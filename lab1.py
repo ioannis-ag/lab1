50ae65edf30e042b7543911d92f7e25e711da5a9
ΑΓΑΘΑΓΓΕΛΟΥ ΙΩΑΝΝΗΣ <up1078532>
2025-02-24 12:51:06 +0200

    check for cookies

--- lab1.py
+++ lab1.py
@@ -21,3 +21,5 @@ with requests.get(url) as response:
     # for key in response.headers:
     #    print(f"{key}, Value:{response.headers[key]}")
     print(f"Server: {response.headers.get('Server')}")
+
+    print(f"Has cookies: {'Set-Cookies' in response.headers}")
